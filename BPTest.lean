9db3255042c4f579b9eab78d3772176ae82e057a
PlanetMacro <mirco@leastauthority.com>
2025-04-03 19:19:46 +0200

    Initial commit for Lean4 project

--- BPTest.lean
+++ BPTest.lean
@@ -1,3 +1,7 @@
 -- This module serves as the root of the `BPTest` library.
 -- Import modules here that should be built as part of the library.
 import BPTest.Basic
+import Mathlib.Data.Real.Basic
+
+
+#check ∀ x : ℝ, 0 ≤ x → |x| = x
